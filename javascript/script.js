cf86cde8999095dbe068b085c57e8731da58272e
Victor Gasperi <victorgasperi@gmail.com>
2025-03-08 07:57:02 -0300

    Initial commit

---
+++ javascript/script.js
@@ -0,0 +1,20 @@
+// const nome = 'José'
+// console.log(nome)
+
+// var linguagem = "Javascript"
+// console.log(linguagem)
+// var linguagem = "Java"
+// console.log(linguagem)
+
+var idade = 18
+
+console.log(nome)
+
+if (idade >= 18) {
+
+    var nome = 'Victor'
+    console.log('Parabéns!')
+
+}
+
+console.log(nome)
