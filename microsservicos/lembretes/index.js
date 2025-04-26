1709ec5c280cb7112b68db4447f2904ff3b9a54f
Victor Gasperi <victorgasperi@gmail.com>
2025-04-26 10:24:58 -0300

    feat(microsservicos): lembretes

---
+++ microsservicos/lembretes/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+
+// define a função middleware do express
+app.use(express.json()) 
+
+
+
+const baseLembretes = {};
+let id = 1;
+
+app.get('/lembretes', (req, res) => {
+    res.json(baseLembretes);
+});
+
+app.post('/lembretes', (req, res) => {
+
+    const { texto } = req.body;
+
+    // É a mesma coisa que
+    // const lembrete = {
+    //     id: id,
+    //     texto: texto
+    // }
+
+    const lembrete = {
+        id,
+        texto
+    }
+
+    baseLembretes[id] = lembrete
+    id++
+
+    res.status(201).json(lembrete)
+
+});
+
+const port = 4000
+
+app.listen(port, () => {
+    console.log(`localhost:4000/lembretes. Porta: ${port}`)
+})
